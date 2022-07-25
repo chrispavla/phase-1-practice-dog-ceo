9e01b76197756e8f3258e885170a0b2c2b881637
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-25 18:41:06 -0400

    commit tree 0e58564cb7baa99f4f25d43ada28ef4e9857a7f3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-console.log('%c HI', 'color: firebrick')
+let imagesContainer = document.querySelector("#dog-image-container");
+let ul = document.querySelector("#dog-breeds");
+let dogBreeds;
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then((res) => res.json())
+  .then((data) => {
+    // let info = Object.entries(data);
+    // console.log(data.message);
+
+    data.message.forEach((dogUrl) => {
+      let image = document.createElement("img");
+      image.setAttribute("src", dogUrl);
+      //   image.src = dogUrl;
+      imagesContainer.appendChild(image);
+    });
+  });
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((res) => res.json())
+  .then((data) => {
+    dogBreeds = Object.keys(data.message);
+
+    dogBreeds.forEach((dogbreed) => {
+      let li = document.createElement("li");
+      li.textContent = dogbreed;
+      ul.appendChild(li);
+
+      li.addEventListener("click", () => {
+        li.style.color === "blue"
+          ? (li.style.color = "black")
+          : (li.style.color = "blue");
+      });
+    });
+  });
+
+let dropdown = document.querySelector("#breed-dropdown");
+
+dropdown.addEventListener("change", (event) => {
+  makeFetch().then((res) => {
+    dogBreeds = Object.keys(res.message);
+
+    let filterArray = dogBreeds.filter((breed) => {
+      return breed.startsWith(event.target.value);
+    });
+
+    ul.innerHTML = "";
+    filterArray.forEach((breed) => {
+      let li = document.createElement("li");
+      li.textContent = breed;
+      ul.appendChild(li);
+
+      li.addEventListener("click", () => {
+        li.style.color === "blue"
+          ? (li.style.color = "black")
+          : (li.style.color = "blue");
+      });
+    });
+  });
+});
+
+function makeFetch() {
+  return fetch("https://dog.ceo/api/breeds/list/all").then((res) => res.json());
+}
